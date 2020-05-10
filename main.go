ab175b8c4ab387f4f4434be2a7243eb6f55bca58
Michel Blanc <mb@devops.works>
2020-05-11 01:07:10 +0200

    Adds env vars & metrics support
    Added environment variables upport
    Added metrics gathering for the executed command

--- main.go
+++ main.go
@@ -2,53 +2,57 @@ package main
 
 import (
 	"bufio"
-	// "bytes"
+	"bytes"
 	"context"
-	// "flag"
 	"fmt"
 	"log"
-
-	// "net/http"
+	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
-
-	// "syscall"
+	"strings"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/crgimenes/goconfig"
 	_ "github.com/crgimenes/goconfig/yaml"
+
+	"github.com/struCoder/pidusage"
 )
 
 type config struct {
-	Timeout float64 `yaml:"timeout" cfg:"timeout"`
-	Influx  influx  `yaml:"influxdb" cfg:"influxdb"`
+	Timeout float64 `yaml:"timeout" cfg:"timeout" cfgDefault:"0" cfgHelper:"command timeout (s); 0 to disable"`
+	Period  float64 `yaml:"period" cfg:"period" cfgHelper:"process consumption sample period (ms)"`
+	Verbose bool    `yaml:"verbose" cfg:"verbose" cfgHelper:"verbose execution"`
+	Influx  influx  `yaml:"influxdb" cfg:"influx" cfgHelper:"influxdb parameters"`
+	command []string
 }
 
 type influx struct {
-	db          string
-	server      string
-	user        string
-	pass        string
-	measurement string
-	tags        []string
-	retries     int
-	timeout     time.Duration
+	DB          string  `yaml:"db" cfg:"db" cfgHelper:"influxdb database"`
+	URL         string  `yaml:"url" cfg:"url" cfgHelper:"influxdb URL"`
+	User        string  `yaml:"user" cfg:"user" cfgHelper:"influxdb user"`
+	Pass        string  `yaml:"pass" cfg:"pass" cfgHelper:"influxdb password"`
+	Measurement string  `yaml:"measurement" cfg:"measurement" cfgHelper:"influxdb measurement name"`
+	Tags        string  `yaml:"tags" cfg:"tags" cfgHelper:"comma-separated influxdb tags (e.g. foo=bar,fizz=buzz)"`
+	Retries     int     `yaml:"retries" cfg:"retries" cfgDefault:"3" cfgHelper:"influxdb retries when writing"`
+	Timeout     float64 `yaml:"timeout" cfg:"timeout" cfgDefault:"5000" cfgHelper:"influxdb writing timeout (ms)"`
+	DryRun      bool    `yaml:"dryrun" cfg:"dryrun" cfgDefault:"false" cfgHelper:"influxdb dry run (runs command but does not log to influxdb)"`
 }
 
 type point struct {
 	measurement string
 	tags        string
-	duration    float64
-	status      int
+	values      map[string]float64
+	timestamp   time.Time
 }
 
 // Version from git sha1/tags
 var Version string
 
-func isInSlice(needle string, stack []string) bool {
-	for _, item := range stack {
-		if item == needle {
+func anyInSlice(needles []string, stack []string) bool {
+	for _, needle := range needles {
+		if slicePosition(needle, stack) != -1 {
 			return true
 		}
 	}
@@ -56,21 +60,18 @@ func isInSlice(needle string, stack []string) bool {
 	return false
 }
 
+func slicePosition(needle string, stack []string) int {
+	for i, item := range stack {
+		if item == needle {
+			return i
+		}
+	}
+
+	return -1
+}
+
 func main() {
-	// var timeout = flag.Float64("timeout", 0, "command timeout (default: 0, no timeout)")
-	// var influxServer = flag.String("server", "", "influxdb server URL (no events are send if not set)")
-	// var influxDB = flag.String("db", "", "influxdb database (no events are send if not set)")
-	// var influxUser = flag.String("user", "", "influxdb username (default: none)")
-	// var influxPass = flag.String("pass", "", "influxdb password (default: none)")
-	// var influxMeasurement = flag.String("measurement", "", "influxdb measurement (default: none, required when server is set)")
-	// var influxTags = flag.String("tags", "", "comma-separated k=v pairs of influxdb tags (default: none, example: 'foo=bar,fizz=buzz')")
-	// var influxRetry = flag.Int("retry", 3, "how many times we try to send the event to influxdb (default: 3)")
-	// var influxTimeout = flag.Int("influxtimeout", 2000, "how many milliseconds do we allow influxdb POST to take (default: 2000)")
-	// var version = flag.Bool("version", false, "show version")
-
-	// flag.Parse()
-
-	if isInSlice("-version", os.Args) {
+	if anyInSlice([]string{"-version", "--version"}, os.Args) {
 		fmt.Printf("%s %s\n", os.Args[0], Version)
 		os.Exit(0)
 	}
@@ -79,83 +80,76 @@ func main() {
 	// step 3: Pass the instance pointer to the parser
 	err := goconfig.Parse(&cfg)
 	if err != nil {
-		println(err)
+		log.Printf("unable to parse arguments: %+v", err)
 		return
 	}
 
-	fmt.Printf("CONFIG: %+v\n", cfg)
+	if cfg.Influx.Retries <= 0 {
+		cfg.Influx.Retries = 3
+	}
 
-	// if *version {
-	// 	fmt.Printf("%s %s\n", os.Args[0], Version)
-	// 	os.Exit(0)
-	// }
-
-	fmt.Printf("ARGS: %+v\n", os.Args)
-
-	if len(os.Args) == 1 {
-		log.Printf("error: no command specified\n")
-		// flag.Usage()
+	pos := slicePosition("--", os.Args)
+	if pos == -1 || pos > len(os.Args)-2 {
+		log.Printf("error: no command specified, use: influxevent [args] -- command...\n")
 		os.Exit(1)
 	}
 
-	if cfg.Influx.retries <= 0 {
-		cfg.Influx.retries = 3
+	cfg.command = os.Args[slicePosition("--", os.Args)+1:]
+
+	run(cfg)
+}
+
+func run(cfg config) {
+	start := time.Now()
+	samples, err := executeCommand(cfg)
+	duration := time.Since(start).Seconds()
+
+	exitStatus := 0
+
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+
+			// This works on both Unix and Windows. Although package
+			// syscall is generally platform dependent, WaitStatus is
+			// defined for both Unix and Windows and in both cases has
+			// an ExitStatus() method with the same signature.
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				exitStatus = status.ExitStatus()
+			}
+		} else {
+			log.Fatalf("error running command: %v", err)
+		}
 	}
 
-	/*
-		start := time.Now()
-		err := executeCommand(flag.Args(), cfg.timeout)
-		duration := time.Since(start).Seconds()
+	if exitStatus == -1 {
+		log.Printf("command has been killed due to timeout")
+	}
 
-		exitStatus := 0
+	if cfg.Influx.URL == "" || cfg.Influx.DB == "" {
+		log.Printf("not writing point to influx since database or url is not set")
+		os.Exit(exitStatus)
+	}
 
-		if err != nil {
-			if exiterr, ok := err.(*exec.ExitError); ok {
-				// The program has exited with an exit code != 0
+	pt := point{
+		measurement: cfg.Influx.Measurement,
+		tags:        "etype=event",
+		values:      map[string]float64{"duration": duration, "status": float64(exitStatus)},
+		timestamp:   start,
+	}
 
-				// This works on both Unix and Windows. Although package
-				// syscall is generally platform dependent, WaitStatus is
-				// defined for both Unix and Windows and in both cases has
-				// an ExitStatus() method with the same signature.
-				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-					exitStatus = status.ExitStatus()
-				}
-			} else {
-				log.Fatalf("error running command: %v", err)
-			}
-		}
+	samples = append(samples, pt)
 
-		if exitStatus == -1 {
-			log.Printf("command has been killed due to timeout")
-		}
+	if cfg.Influx.DryRun {
+		err = batchLogInfluxDB(dumpInfluxDB, cfg.Influx, samples)
+	} else {
+		err = batchLogInfluxDB(logInfluxDB, cfg.Influx, samples)
+	}
 
-		if *influxServer == "" || *influxDB == "" {
-			log.Printf("not writing point to influx since database or url is not set (%d)", exitStatus)
-			os.Exit(exitStatus)
-		}
-
-		inf := influx{
-			db:      *influxDB,
-			url:     *influxServer,
-			user:    *influxUser,
-			pass:    *influxPass,
-			retries: *influxRetry,
-			timeout: time.Duration(*influxTimeout) * time.Millisecond,
-		}
-
-		pt := point{
-			measurement: *influxMeasurement,
-			tags:        *influxTags,
-			duration:    duration,
-			status:      exitStatus,
-		}
-
-		err = logInfluxDB(inf, pt)
-		if err != nil {
-			log.Printf("unable to write to influxdb: %v", err)
-			os.Exit(1)
-		}
-	*/
+	if err != nil {
+		log.Printf("unable to write to influxdb: %v", err)
+		os.Exit(1)
+	}
 }
 
 func (p point) String() string {
@@ -168,35 +162,80 @@ func (p point) String() string {
 	if p.tags != "" {
 		influxString = fmt.Sprintf("%s,%s", influxString, p.tags)
 	}
-	influxString = fmt.Sprintf("%s duration=%s,status=%d", influxString, strconv.FormatFloat(p.duration, 'f', -1, 64), p.status)
+
+	values := []string{}
+	for k, v := range p.values {
+		values = append(values, fmt.Sprintf("%s=%g", k, v))
+	}
+	influxString = fmt.Sprintf("%s %s %d", influxString, strings.Join(values, ","), p.timestamp.UnixNano())
+
 	return influxString
 }
 
-/*
-func logInfluxDB(server influx, point point) error {
-	buf := bytes.NewBufferString(point.String())
+func batchLogInfluxDB(fn func(influx, []byte) error, cfg influx, points []point) error {
+	lines := []byte{}
+	for i, pt := range points {
+		pt.measurement = cfg.Measurement
+		pt.tags += "," + cfg.Tags
+		pt.tags = strings.Trim(pt.tags, ",")
 
+		lines = append(lines, []byte(pt.String())...)
+		lines = append(lines, '\n')
+		if (i+1)%500 == 0 {
+			err := fn(cfg, lines)
+			if err != nil {
+				return err
+			}
+			lines = []byte{}
+		}
+	}
+
+	// Send remainder
+	return fn(cfg, lines)
+}
+
+func dumpInfluxDB(cfg influx, lines []byte) error {
 	var uri string
-	if server.url[len(server.url)-1] == '/' {
-		uri = fmt.Sprintf("%swrite?db=%s", server.url, server.db)
+	if cfg.URL[len(cfg.URL)-1] == '/' {
+		uri = fmt.Sprintf("%swrite?db=%s", cfg.URL, cfg.DB)
 	} else {
-		uri = fmt.Sprintf("%s/write?db=%s", server.url, server.db)
+		uri = fmt.Sprintf("%s/write?db=%s", cfg.URL, cfg.DB)
 	}
 
 	// Dangerous; shoud use url encoding
-	if server.user != "" {
-		uri += fmt.Sprintf("&u=%s&p=%s", server.user, server.pass)
+	if cfg.User != "" {
+		uri += fmt.Sprintf("&u=%s&p=%s", cfg.User, cfg.Pass)
+	}
+
+	fmt.Println(uri)
+	fmt.Println(string(lines))
+	return nil
+}
+
+func logInfluxDB(cfg influx, lines []byte) error {
+	var uri string
+	if cfg.URL[len(cfg.URL)-1] == '/' {
+		uri = fmt.Sprintf("%swrite?db=%s", cfg.URL, cfg.DB)
+	} else {
+		uri = fmt.Sprintf("%s/write?db=%s", cfg.URL, cfg.DB)
+	}
+
+	// Dangerous; shoud use url encoding
+	if cfg.User != "" {
+		uri += fmt.Sprintf("&u=%s&p=%s", cfg.User, cfg.Pass)
 	}
 
 	client := &http.Client{
-		Timeout: server.timeout,
+		Timeout: time.Duration(cfg.Timeout) * time.Millisecond,
 	}
 
 	var err error
 	var resp *http.Response
 
-	for try := 0; try < server.retries; try++ {
-		resp, err = client.Post(uri, "application/x-www-form-urlencoded", buf)
+	r := bytes.NewReader(lines)
+
+	for try := 0; try < cfg.Retries; try++ {
+		resp, err = client.Post(uri, "application/x-www-form-urlencoded", r)
 		if err == nil {
 			break
 		}
@@ -207,23 +246,22 @@ func logInfluxDB(server influx, point point) error {
 	}
 
 	if resp.StatusCode != 204 {
-		return fmt.Errorf("unable to write to influxdb server %s, got response: %s", server.url, resp.Status)
+		return fmt.Errorf("unable to write to influxdb server %s, got response: %s", cfg.URL, resp.Status)
 	}
 	return nil
 }
-*/
 
-func executeCommand(args []string, timeout float64) error {
+func executeCommand(cfg config) ([]point, error) {
 	var cmd *exec.Cmd
 	var ctx context.Context
 	var cancel context.CancelFunc
 
-	if timeout != 0 {
-		ctx, cancel = context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	if cfg.Timeout != 0 {
+		ctx, cancel = context.WithTimeout(context.Background(), time.Duration(cfg.Timeout)*time.Second)
 		defer cancel()
-		cmd = exec.CommandContext(ctx, args[0], args[1:]...)
+		cmd = exec.CommandContext(ctx, cfg.command[0], cfg.command[1:]...)
 	} else {
-		cmd = exec.Command(args[0], args[1:]...)
+		cmd = exec.Command(cfg.command[0], cfg.command[1:]...)
 	}
 
 	cmd.Env = append(os.Environ())
@@ -238,31 +276,74 @@ func executeCommand(args []string, timeout float64) error {
 	}
 
 	scanOut := bufio.NewScanner(sout)
+	scanErr := bufio.NewScanner(serr)
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for scanOut.Scan() {
-			fmt.Printf("out: %s\n", scanOut.Text())
+			fmt.Fprintf(os.Stdout, "%s\n", scanOut.Text())
 		}
 	}()
 
-	scanErr := bufio.NewScanner(serr)
-
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for scanErr.Scan() {
-			fmt.Printf("err: %s\n", scanErr.Text())
+			fmt.Fprintf(os.Stderr, "%s\n", scanErr.Text())
 		}
 	}()
 
 	err = cmd.Start()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	log.Printf("Just ran subprocess %d\n", cmd.Process.Pid)
+	if cfg.Verbose {
+		log.Printf("started subprocess %d\n", cmd.Process.Pid)
+	}
 
-	return cmd.Wait()
+	samplec := make(chan point, 500)
+	var samples []point
+
+	// Monitor process is a period is specified
+	if cfg.Period > 0 {
+		wg.Add(1)
+		go watch(cmd.Process.Pid, cfg.Period, samplec, &wg)
+
+		for s := range samplec {
+			s.tags = "etype=metric," + s.tags
+			s.tags = strings.Trim(s.tags, ",")
+			samples = append(samples, s)
+		}
+	}
+
+	wg.Wait()
+
+	return samples, cmd.Wait()
 }
 
-func watch(pid int) {
-	// see https://github.com/struCoder/pidusage/blob/master/pidusage.go
+func watch(pid int, period float64, c chan point, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(c)
+
+	ticker := time.NewTicker(time.Duration(period) * time.Millisecond)
+
+	for {
+		t := <-ticker.C
+		sysInfo, err := pidusage.GetStat(pid)
+		if err != nil || sysInfo.Memory == 0 {
+
+			return
+		}
+		c <- point{
+			values: map[string]float64{
+				"cpu":    sysInfo.CPU,
+				"memory": sysInfo.Memory,
+			},
+			timestamp: t,
+		}
+	}
 }
